c05ac5cc32391cd7b351242005add99f3705ad37
kehvarl <kehvarl@kehvarl.com>
2021-02-06 12:19:30 -0700

    register todo app

--- backend/todo_project/settings.py
+++ backend/todo_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+
+
+    # Local
+    'todo.apps.TodoConfig',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
